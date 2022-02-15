781cb2b2e6c91827188734d8c2a2caad3dac8429
Jolan Thomassin <98430140+JolanThomassin@users.noreply.github.com>
2022-02-15 04:49:22 +0100

    Conversion de quelque texte Anglais en Français

--- Programme/Bot/pybot.py
+++ Programme/Bot/pybot.py
@@ -1,15 +1,23 @@
+### Interface graphique ###
 from tkinter import *
+### IA / Bot ###
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
+### Copie presse papier ###
 import pyperclip
+### Youtube ###
 import urllib.request
 import re
+### Météo ###
+from requests_html import HTMLSession
 
 class Bot() :
 	def __init__(self):
 		### Paramètres ###
+		# Définition des dimensions de l'écran #
 		self.largeurEcran = 720
-		self.hauteurEcran = 480 
+		self.hauteurEcran = 480
+		# Création de l'écran #
 		self.ecranLogiciel = Tk()
 		self.ecranLogiciel.title("Le Botchat de Jolan")
 		self.ecranLogiciel.geometry("720x480+200+40")
@@ -86,6 +94,16 @@ class Bot() :
 			'https://github.com/cegepmatane/projet-specialise-2022-JolanThomassin',
 		])
 
+		### Train pour la météo ###
+		trainer.train([
+		 	'Météo',
+			'Voici la température dans votre région : ',
+		])
+		trainer.train([
+		 	'Température',
+			'Voici la température dans votre région : ',
+		])
+
 		### Train pour Youtube ###
 		trainer.train([
 		 	'Youtube',
@@ -97,69 +115,65 @@ class Bot() :
 		self.ecranLogiciel.mainloop()
 
 	def pagePrincipale(self):
-		### Reset ###
+		### Reset de l'écran ###
 		self.canvaEcranLogiciel.delete(ALL)
 
-		### Images ###
-		self.BackgroundDécor = PhotoImage(file="background.png")
-		self.canvaEcranLogiciel.create_image(self.largeurEcran / 2, self.hauteurEcran / 2, image=self.BackgroundDécor)
-
-		### Boutons ###
-		self.boutonQuitter = Button(self.ecranLogiciel, text=" Quitter ", font=("OCR A Extended", 15), bg='#ffe5ea', fg='black', command=self.QuitterLejeux)
-		self.boutonQuitter = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)-285, 450, window=self.boutonQuitter)
+		### Images du chatbot ###
+		self.imageDeFond = PhotoImage(file="background.png")
+		self.canvaEcranLogiciel.create_image(self.largeurEcran / 2, self.hauteurEcran / 2, image=self.imageDeFond)
 
 		### Fonction ###
 		def récupérationDuTexte():
-			texte = un_entry.get()
+			texte = entreeUtilisateur.get()
 			if self.modeRechercheYoutubeActif == False :
 				if (texte != "Entrez un message" and texte != "") :
-					response = self.chatbot.get_response(texte)
-					if str(response) == 'Quel vidéo/musique cherchez vous ?' :
+					reponseDuBot = self.chatbot.get_response(texte)
+					if str(reponseDuBot) == 'Quel vidéo/musique cherchez vous ?' :
 						self.modeRechercheYoutubeActif = True
-					self.changerDeTexte(texte, response)
+					elif str(reponseDuBot) == 'Voici la température dans votre région :' :
+						reponseDuBot = 'Voici la température dans votre région : ' + str(self.getMétéo()) + '°C' 
+					self.changerDeTexte(texte, reponseDuBot)
 					self.pagePrincipale()
 			else :
-				url = self.rechercheVideoSurYoutube(texte)
+				lienVideoYoutube = self.rechercheVideoSurYoutube(texte)
 				self.modeRechercheYoutubeActif = False
-				self.changerDeTexte(texte, url)
+				self.changerDeTexte(texte, lienVideoYoutube)
 				self.pagePrincipale()
 				
-		def entry_clear(e):
-			un_entry.delete(0, END)
+		def suppressionTexteBarreDeSaisie(e):
+			entreeUtilisateur.delete(0, END)
 
-		### Saisie  ###
-		un_entry = Entry(self.ecranLogiciel, font=("Helvectica", 24), width=20, fg="#336d92", bd=0)
-		un_window = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)-10, 425, anchor="n", height=45, width=410,window=un_entry)
-		un_entry.insert(0, "Entrez un message")
-		un_entry.bind("<Button-1>", entry_clear)
+		### Saisie ###
+		entreeUtilisateur = Entry(self.ecranLogiciel, font=("Helvectica", 24), width=20, fg="#336d92", bd=0)
+		barreDeSaisie = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)-10, 425, anchor="n", height=45, width=410,window=entreeUtilisateur)
+		entreeUtilisateur.insert(0, "Entrez un message")
+		entreeUtilisateur.bind("<Button-1>", suppressionTexteBarreDeSaisie)
 
-		### Boutons ###
-		self.boutonSendMessage = Button(self.ecranLogiciel, text="Send", font=("OCR A Extended", 15), bg='#5CB6AD', fg='black', command=récupérationDuTexte)
-		self.boutonSendMessage = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)+245, 450, window=self.boutonSendMessage)
-		self.boutonCopy = Button(self.ecranLogiciel, text="Copy", font=("OCR A Extended", 15), bg='#5CB6FF', fg='black', command=self.copierLeTexte)
-		self.boutonCopy = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)+320, 450, window=self.boutonCopy)
+		### Boutons du chatbot ###
+		self.boutonEnvoieTexte = Button(self.ecranLogiciel, text="Send", font=("OCR A Extended", 15), bg='#5CB6AD', fg='black', command=récupérationDuTexte)
+		self.boutonEnvoieTexte = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)+245, 450, window=self.boutonEnvoieTexte)
+		self.boutonDeCopie = Button(self.ecranLogiciel, text="Copy", font=("OCR A Extended", 15), bg='#5CB6FF', fg='black', command=self.copierLeTexte)
+		self.boutonDeCopie = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)+320, 450, window=self.boutonDeCopie)
+		self.boutonQuitterApplication = Button(self.ecranLogiciel, text=" Quitter ", font=("OCR A Extended", 15), bg='#ffe5ea', fg='black', command=self.quitterLeJeu)
+		self.boutonQuitterApplication = self.canvaEcranLogiciel.create_window((self.largeurEcran/2)-285, 450, window=self.boutonQuitterApplication)
 
-		### Textes ###
-		self.résultaCode = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+150, text="", font=("OCR A Extended", 30), fill="#1CF8FF")
+		### Chat de l'application ###
+		self.chatUserPositionUn = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+150, text=self.texteUn, font=("OCR A Extended", 14), fill="#EAD0E2")
+		self.chatUserPositionDeux = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+100, text=self.texteDeux, font=("OCR A Extended", 14), fill="#6CB6BB")
+		self.chatUserPositionTrois = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+50, text=self.texteTrois, font=("OCR A Extended", 14), fill="#EAD0E2")
+		self.chatUserPositionQuatre = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2), text=self.texteQuatre, font=("OCR A Extended", 14), fill="#6CB6BB")
+		self.chatUserPositionCinq = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)-50, text=self.texteCinq, font=("OCR A Extended", 14), fill="#EAD0E2")
+		self.chatUserPositionSix = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2-100), text=self.texteSix, font=("OCR A Extended", 14), fill="#6CB6BB")
+		self.chatUserPositionSept = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)-150, text=self.texteSept, font=("OCR A Extended", 14), fill="#EAD0E2")
+		self.chatUserPositionHuit = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2-200), text=self.texteHuit, font=("OCR A Extended", 14), fill="#6CB6BB")
 
-		### Tchat ###
-		self.chatUserPositionUn = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+150, text=self.texteUn, font=("OCR A Extended", 22), fill="#EAD0E2")
-		self.chatUserPositionDeux = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+100, text=self.texteDeux, font=("OCR A Extended", 22), fill="#6CB6BB")
-		self.chatUserPositionTrois = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)+50, text=self.texteTrois, font=("OCR A Extended", 22), fill="#EAD0E2")
-		self.chatUserPositionQuatre = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2), text=self.texteQuatre, font=("OCR A Extended", 22), fill="#6CB6BB")
-		self.chatUserPositionCinq = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)-50, text=self.texteCinq, font=("OCR A Extended", 22), fill="#EAD0E2")
-		self.chatUserPositionSix = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2-100), text=self.texteSix, font=("OCR A Extended", 22), fill="#6CB6BB")
-		self.chatUserPositionSept = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2)-150, text=self.texteSept, font=("OCR A Extended", 22), fill="#EAD0E2")
-		self.chatUserPositionHuit = self.canvaEcranLogiciel.create_text((self.largeurEcran/2), (self.hauteurEcran/2-200), text=self.texteHuit, font=("OCR A Extended", 22), fill="#6CB6BB")
-
-	def QuitterLejeux(self):
+	def quitterLeJeu(self):
 		self.ecranLogiciel.destroy()
 
 	def copierLeTexte(self):
 		pyperclip.copy(str(self.texteUn))
-		spam = pyperclip.paste()
 
-	def changerDeTexte(self, texte, response):
+	def changerDeTexte(self, texte, reponseDuBot):
 		self.texteHuit = self.texteSix
 		self.texteSept = self.texteCinq
 		self.texteSix = self.texteQuatre
@@ -167,14 +181,21 @@ class Bot() :
 		self.texteQuatre = self.texteDeux
 		self.texteTrois = self.texteUn
 		self.texteDeux = texte
-		self.texteUn = response
+		self.texteUn = reponseDuBot
 
 	def rechercheVideoSurYoutube(self, texte) :
-		search_keyword = texte
-		new_string = search_keyword.replace(" ", "+")
-		html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + new_string)
-		video_ids = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-		return("https://www.youtube.com/watch?v=" + video_ids[0])
+		motRecherche = texte
+		new_string = motRecherche.replace(" ", "+")
+		lienRechercheVideo = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + new_string)
+		lienVideo = re.findall(r"watch\?v=(\S{11})", lienRechercheVideo.read().decode())
+		return("https://www.youtube.com/watch?v=" + lienVideo[0])
+
+	def getMétéo(self):
+		sessionWEB = HTMLSession()
+		lieuRecherche = 'matane'
+		url = f'https://www.google.com/search?q=weather+{lieuRecherche}'
+		navigateurUtilise = sessionWEB.get(url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36 OPR/82.0.4227.50'})
+		return(navigateurUtilise.html.find('span#wob_tm', first=True).text)
 
 lancementLogiciel = Bot()
 lancementLogiciel
