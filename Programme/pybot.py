6efe68fa78f3092ea98e27e5943d4f2575dd48f6
Jolan Thomassin <98430140+JolanThomassin@users.noreply.github.com>
2022-03-18 13:37:43 -0400

    Algorithme d'apprentissage automatique #14
    Issue référencé : #14
    En écrivant "lancement training", on pourra désormais envoyer une cinquantaine de messages aléatoires au bot pour l'entrainer.

--- Programme/pybot.py
+++ Programme/pybot.py
@@ -10,6 +10,9 @@ import urllib.request
 import re
 ### Météo ###
 from requests_html import HTMLSession
+### Auto training ###
+import time
+import random
 
 class Bot() :
 	def __init__(self):
@@ -36,44 +39,84 @@ class Bot() :
 		self.texteHuit = ""
 		self.modeRechercheYoutubeActif = False
 		self.police = 14
+		self.timer = 0
 
 		### Création du bot ###
 		self.chatbot = ChatBot("Jolan")
 		trainer = ListTrainer(self.chatbot)
 		
-		### Train pour Début de conversation ###
+		### Train en cours ###
+		### Train pour Loisirs / Passions ###
+		### Loisirs ###
 		trainer.train([
-		 	'Salut',
-			'Salut, Comment allez-vous ?',
-			'Je vais bien',
-			'Que puis-je faire pour vous ?',
+		 	'Quel sont tes loisirs ?',
+			"J'aime la programmation et regarder des films, et vous ?",
 		])
 		trainer.train([
-		 	'Hey',
-			'Salut, Comment allez-vous ?',
-			'Je vais bien',
-			'Que puis-je faire pour vous ?',
+		 	'Loisirs ?',
+			"J'aime la programmation et regarder des films, et vous ?",
 		])
 		trainer.train([
-		 	'Wesh',
-			'Salut, Comment allez-vous ?',
-			'ca va',
-			'Que puis-je faire pour vous ?',
+		 	'As tu des loisirs ?',
+			"J'aime la programmation et regarder des films, et vous ?",
 		])
 		trainer.train([
-		 	'Bonjour',
-			'Salut, Comment allez-vous ?',
-			'Super',
-			'Que puis-je faire pour vous ?',
+		 	"Comment t'occupes tu ?",
+			"J'aime la programmation et regarder des films, et vous ?",
 		])
 		trainer.train([
-		 	'Bonsoir',
-			'Salut, Comment allez-vous ?',
-			'Bien',
-			'Que puis-je faire pour vous ?',
+		 	"Que fais tu ?",
+			"Je parle avec vous",
+		])
+
+		### Passions ###
+		trainer.train([
+		 	'Quel sont tes passions ?',
+			"J'aime la programmation et regarder des filmset vous ?",
+		])
+		trainer.train([
+		 	"Qu'aimes tu faires ?",
+			"J'aime la programmation et regarder des filmset vous ?",
+		])
+		trainer.train([
+		 	"Passions ?",
+			"J'aime la programmation et regarder des filmset vous ?",
+		])
+
+		### Film ###
+		trainer.train([
+		 	'Quel genre de film aime tu ?',
+			"J'aime les films de gladiateurs, et vous ?",
+		])
+		trainer.train([
+		 	'Tu aimes les films ?',
+			"J'aime les films de gladiateurs, et vous ?",
+		])
+		trainer.train([
+		 	'Films ?',
+			"J'aime les films de gladiateurs, et vous ?",
+		])
+
+		### Réponses ###
+		trainer.train([
+		 	'Moi aussi',
+			"Nous avons tous deux bon gouts alors.",
+		])
+		trainer.train([
+		 	'Idem',
+			"Nous avons tous deux bon gouts alors.",
+		])
+		trainer.train([
+		 	"Je n'aimes pas ça",
+			"Notre différence fait notre force !",
+		])
+		trainer.train([
+		 	"Je préfères autre choses",
+			"Notre différence fait notre force !",
 		])
 
 		### Train pour Présentation ###
+		### Nom et rôle ###
 		trainer.train([
 		 	'Présente toi',
 			'Bonjour je suis Deep un chatbot réalisé en Python',
@@ -90,39 +133,40 @@ class Bot() :
 		 	'Présentation',
 			'Bonjour je suis Deep un chatbot réalisé en Python',
 		])
-
-		### Train pour Loisirs / Passions ###
 		trainer.train([
-		 	'Quel sont tes loisirs ?',
-			"J'aime la programmation et regarder des films",
-			"Quel film ?",
-			"Les films de gladiateurs",
-		])
-		trainer.train([
-			"Quel genre de film ?",
-			"Les films de gladiateurs",
-		])
-		trainer.train([
-		 	'loisirs ?',
-			"J'aime la programmation et regarder des films",
-		])
-		trainer.train([
-		 	'Quesque tu aimes faire ?',
-			"J'aime la programmation et regarder des films",
-		])
-		trainer.train([
-		 	'Quesque tu aimes ?',
-			"J'aime la programmation et regarder des films",
-		])
-		trainer.train([
-		 	'Quel film aimes tu ?',
-			"J'aime les films de gladiateurs",
-		])
-		trainer.train([
-		 	'Quel sont tes passions ?',
-			"J'aime les films de gladiateurs",
+		 	'Tu es qui ?',
+			'Bonjour je suis Deep un chatbot réalisé en Python',
 		])
 
+		### Âge ###
+		trainer.train([
+		 	'Quel âge as tu ?',
+			'Je suis né le 25 janvier 2022',
+		])
+		trainer.train([
+		 	'Quel es ta date de naissance ?',
+			'Je suis né le 25 janvier 2022',
+		])
+		trainer.train([
+		 	'Quand es tu né ?',
+			'Bonjour je suis Deep un chatbot réalisé en Python',
+		])
+		trainer.train([
+		 	'En quel année tu es né ?',
+			'Bonjour je suis Deep un chatbot réalisé en Python',
+		])
+		trainer.train([
+		 	'En quel mois tu es né ?',
+			'Bonjour je suis Deep un chatbot réalisé en Python',
+		])
+		trainer.train([
+		 	'Quel mois tu es né ?',
+			'Bonjour je suis Deep un chatbot réalisé en Python',
+		])
+		trainer.train([
+		 	'Quel jours tu es né ?',
+			'Bonjour je suis Deep un chatbot réalisé en Python',
+		])
 		### Train pour GIT ###
 		trainer.train([
 		 	'Git',
@@ -144,34 +188,130 @@ class Bot() :
 		 	'ton git',
 			'https://github.com/cegepmatane/projet-specialise-2022-JolanThomassin',
 		])
-
+		### Train pour Humour ###
+		trainer.train([
+		 	'Raconte moi une blague',
+			'La plage dit à l’océan : dire que tout le monde aime l’eau c’est assez vague',
+		])
+		trainer.train([
+		 	'Raconte moi une blague',
+			'C’est un panda qui en avait marre de la vie et un jour, il se panda…',
+		])
+		trainer.train([
+		 	'Raconte moi une blague',
+			'Que fait un poussin de 200kg ? PIOUUUU! PIOUUUU',
+		])
 		### Train pour la météo ###
 		trainer.train([
 		 	'Météo',
 			'Voici la température dans votre région : ',
 		])
 		trainer.train([
+		 	'météo',
+			'Voici la température dans votre région : ',
+		])
+		trainer.train([
 		 	'Température',
 			'Voici la température dans votre région : ',
 		])
-
+		trainer.train([
+		 	'température',
+			'Voici la température dans votre région : ',
+		])
+		trainer.train([
+		 	'Quel est la météo',
+			'Voici la température dans votre région : ',
+		])
+		trainer.train([
+		 	'Quel est la température',
+			'Voici la température dans votre région : ',
+		])
+		trainer.train([
+		 	'Quel est le temps de dehors ?',
+			'Voici la température dans votre région : ',
+		])
+		trainer.train([
+		 	'Quel est le temps de dehors',
+			'Voici la température dans votre région : ',
+		])
 		### Train pour Youtube ###
 		trainer.train([
 		 	'Youtube',
 			'Quel vidéo/musique cherchez vous ?',
 		])
-
-		### Train pour Loisirs ###
 		trainer.train([
-		 	'Quels sont tes loisirs ?',
-			'Discuter avec vous.',
+		 	'Ytb',
+			'Quel vidéo/musique cherchez vous ?',
+		])
+		trainer.train([
+		 	'YouTube',
+			'Quel vidéo/musique cherchez vous ?',
+		])
+		trainer.train([
+		 	'Je veux faire une recherche youtube',
+			'Quel vidéo/musique cherchez vous ?',
+		])
+		trainer.train([
+		 	'Recherche youtube',
+			'Quel vidéo/musique cherchez vous ?',
+		])
+		trainer.train([
+		 	'Recherche Youtube',
+			'Quel vidéo/musique cherchez vous ?',
+		])
+		trainer.train([
+		 	'Recherche YouTube',
+			'Quel vidéo/musique cherchez vous ?',
+		])
+		### Train pour Début ###
+		### Salutation ###
+		trainer.train([
+		 	'Salut',
+			'Salut, Comment allez-vous ?',
+		])
+		trainer.train([
+		 	'Hey',
+			'Salut, Comment allez-vous ?',	
+		])
+		trainer.train([
+		 	'Wesh',
+			'Salut, Comment allez-vous ?',
+		])
+		trainer.train([
+		 	'Bonjour',
+			'Salut, Comment allez-vous ?',
+		])
+		trainer.train([
+		 	'Bonsoir',
+			'Salut, Comment allez-vous ?',
+		])
+		trainer.train([
+		 	'Allo',
+			'Salut, Comment allez-vous ?',
+		])
+		trainer.train([
+		 	'Bon matin',
+			'Salut, Comment allez-vous ?',
 		])
 
-		### Train pour Humour ###
+		### État ###
 		trainer.train([
-		 	'Raconte moi une blague',
-			'La plage dit à l’océan : dire que tout le monde aime l’eau c’est assez vague',
+		 	'Je vais bien',
+			'Heureux pour vous ! Que puis-je faire pour vous ?',
 		])
+		trainer.train([
+		 	'Je vais mal',
+			'Dommage, que puis-je faire pour vous ?',
+		])
+		trainer.train([
+		 	'Bien',
+			'Que puis-je faire pour vous ?',
+		])
+		trainer.train([
+		 	'Mal',
+			'Dommage, que puis-je faire pour vous ?',
+		])
+
 		### Démarrage ###
 		self.pagePrincipale()
 		self.ecranLogiciel.mainloop()
@@ -186,16 +326,33 @@ class Bot() :
 
 		### Fonction ###
 		def récupérationDuTexte():
+			self.timer = 0
 			texte = entreeUtilisateur.get()
 			if self.modeRechercheYoutubeActif == False :
 				if (texte != "Entrez un message" and texte != "") :
-					reponseDuBot = self.chatbot.get_response(texte)
-					if str(reponseDuBot) == 'Quel vidéo/musique cherchez vous ?' :
-						self.modeRechercheYoutubeActif = True
-					elif str(reponseDuBot) == 'Voici la température dans votre région :' :
-						reponseDuBot = 'Voici la température dans votre région : ' + str(self.getMétéo()) + '°C' 
-					self.changerDeTexte(texte, reponseDuBot)
-					self.pagePrincipale()
+					if (texte == "lancement training") :
+						def autoTrain() :
+							while self.timer <= 50 :
+								texte = ["Hey","Salut","Bonjour","Wesh","Bon matin","Allo", "Je vais bien",
+								"Je vais mal", "Raconte moi une blague", "En quel année tu es né ?", "Quel es ta date de naissance ?",
+								"Quel âge as tu ?","Quel mois tu es né ?","Tu es qui ?","Présente toi","Tu aimes les films ?",
+								"Qu'aimes tu faires ","Quel sont tes passions ?","Que fais tu ?","As tu des loisirs ?",
+								"Quel sont tes loisirs ?"]
+								valeurAleatoire = random.randint(0, (len(texte)-1) )
+								reponseDuBot = self.chatbot.get_response(texte[valeurAleatoire])
+								self.changerDeTexte(texte[valeurAleatoire], reponseDuBot)
+								self.pagePrincipale()
+								self.timer += 1
+								self.canvaEcranLogiciel.after(10, autoTrain)
+						autoTrain()
+					else :
+						reponseDuBot = self.chatbot.get_response(texte)
+						if str(reponseDuBot) == 'Quel vidéo/musique cherchez vous ?' :
+							self.modeRechercheYoutubeActif = True
+						elif str(reponseDuBot) == 'Voici la température dans votre région :' :
+							reponseDuBot = 'Voici la température dans votre région : ' + str(self.getMétéo()) + '°C' 
+						self.changerDeTexte(texte, reponseDuBot)
+						self.pagePrincipale()
 			else :
 				lienVideoYoutube = self.rechercheVideoSurYoutube(texte)
 				self.modeRechercheYoutubeActif = False
